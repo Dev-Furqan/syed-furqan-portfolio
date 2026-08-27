c1b8398d0b5a48371066875149fb9c348f5cfba6
Dev-Furqan <syedfurqan1887@gmail.com>
2026-08-28 02:26:27 +0500

    color changes smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import { HelmetData } from './constants/site';
 import AppRoutes from './routes/AppRoutes';
+import { useLenis } from './hooks/useLenis';
 
 export default function App() {
+  useLenis();
 
   useEffect(() => {
     document.title = HelmetData.title;
